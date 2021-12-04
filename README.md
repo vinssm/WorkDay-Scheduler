d9991cb69f81bd54bfc91fa8a98a3a418a224b40
Vinay Vallabhaneni <vinssmedia@gmail.com>
2021-12-04 09:30:20 -0500

    workday scheduler app completed

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # WorkDay-Scheduler
 ## A daily planner to create a schedule.
-### This is a simple 9 hpour day calendar scheduler to save your events as you plan accordingly. The browser takes the inputs from the user and saves it to the local storage.
+### This is a simple 9 hour day calendar scheduler to save your events as you plan accordingly. The browser takes the inputs from the user and saves it to the local storage.
 ### The input values from the user will remain untill the user clears/delete the local storage.
 ### We are also auditing the time and doing color coding for each hour to display the colors accordingly.
 ### This App is build with HTML, CSS, Java Script, jquery, Moment.js, Bootstrap classes, google fonts, open iconic for icons, popper.js, Web API - DOM Element Properties and local storage for storing the user input values.
---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+// get current date
+var currentDay = moment().format("dddd, MMMM Do");
+$("#currentDay").text(currentDay);
+
+var currentHour = parseInt(moment().format("H"));
+
+// functions
+audit()
+workDay()
+
+
+// workday function to save the input values and shows on the scheduler even when you refresh the page
+function workDay() {
+       $(".time").each(function() {
+        var presentTime = $(this).text();
+        var presentInput = localStorage.getItem(presentTime);
+            console.log(this);
+        if (presentInput !== null) {
+            $(this).siblings(".formInput").val(presentInput)
+            $("this").siblings().css("border", "1px solid blue")
+        }
+     });
+}
+
+// button click to save user input data
+$(".btnSave").on("click", function() {                
+  console.log($("time").parent()); 
+// highlights the sibling and next elements
+    $("#div3").siblings().css("border", "1px solid red")
+    $("#div3").next().css("border", "1px solid blue")
+    $("#div2").siblings().css("border", "1px solid yellow")
+  var timeSection = $(this).siblings(".time").text();
+  var formInput = $(this).siblings(".formInput").val();
+  console.log(this); 
+// storing data into local storage
+  localStorage.setItem(timeSection, formInput);
+// console log to print on console
+    console.log($(this).val());       
+       console.log(formInput) 
+});
+
+
+// audit the current time so that it color codes accordingly
+function audit() {
+    // convert to moment object 
+     var time = moment().hours();
+// for each input we are checking time and adding the color class
+   $(".formInput").each(function() {
+       var presentTime = parseInt($(this).attr("taskID"));
+       if (presentTime > time) {
+        $(this).addClass("list-group-item-primary");
+       } else if (presentTime === time) {
+        $(this).addClass("list-group-item-warning");
+       } else {
+        $(this).addClass("list-group-item-danger");
+       }
+   })
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
